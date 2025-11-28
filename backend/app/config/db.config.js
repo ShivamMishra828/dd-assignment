ad56fdcbfbaf96df97a0f12e150d2f2f6ca78773
ShivamMishra828 <shivam9878522087@gmail.com>
2025-11-28 10:21:22 +0530

    feat: added Dockerfile for frontend and backend with docker compose

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://localhost:27017/dd_db"
+    url: "mongodb://admin:123456@db:27017/dd_db?authSource=admin",
 };
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-    origin: "*",
-}));
+app.use(
+    cors({
+        origin: true,
+    })
+);
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -15,21 +17,18 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+    .connect(db.url)
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch((err) => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Test application." });
+    res.json({ message: "Welcome to Test application." });
 });
 
 require("./app/routes/turorial.routes")(app);
@@ -37,5 +36,5 @@ require("./app/routes/turorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
